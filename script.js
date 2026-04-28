d8fb19add35ad9e3653368f7f78b3dd95198f97b
Darsh Pratap Singh <dpsingh1@my.waketech.edu>
2026-04-28 14:07:58 -0400

    day 2

--- script.js
+++ script.js
@@ -64,38 +64,92 @@ class Grid {
 
 
 }
-
+let direction;
+let snakePos = []
 class Snake extends Grid {
     constructor() {
         super();
-        console.log(this.lengthInput)
-        console.log("smth123")
+        this.length = 1;
+        this.apple = new Apple();
+
         this.start_game.addEventListener("click", () =>{
+            // this.start_game.style.display = "none";
             this.Start_Position()
+            this.game = setInterval(this.Movment, 1000)
         })
         
     }
 
     Start_Position() {
         this.table = document.getElementById("table")
-        let X = Math.floor(Math.random() * (this.widthInput.value));
-        console.log(X)
-        let Y = Math.floor(Math.random() * (this.lengthInput.value));
-        console.log(Y)
-        this.table.rows[Y].cells[X].className = "snake_current"
-        console.log(this.table.rows[Y].cells[X])
-        console.log(this.table.rows[Y].cells[X].class)
+        let x = Math.floor(Math.random() * (this.widthInput.value));
+        let y = Math.ceil(Math.random() * (this.lengthInput.value));
+
+        this.table.rows[y-1].cells[x-1].className = "snake_current"
+       
+        snakePos.push([x,y])
+        this.apple.SpawnApple(snakePos);
         // this.table.rows[Y].cells[X].style.backgroundColor = "darkgreen";
-        console.log(this.table.rows[Y].cells[X])
 
 
+    }
+
+    Movment() {
+        document.addEventListener("keydown", (event) => {
+            console.log('1235')
+            if (event.code === "ArrowUp" || event.key === "w") {
+                console.log("w clicked")
+                direction = "up"
+                console.log(direction)
+            } else if (event.code === "ArrowDown" || event.key === "s") {
+                direction = "down"
+            } else if (event.code === "ArrowLeft" || event.key === "a") {
+                direction = "left"
+            } else if (event.code === "ArrowRight" || event.key === "d") {
+                direction = "right"
+            } else {
+                console.log("should end")
+                clearInterval(this.game)
+            }
+        })
+        console.log('123')
+        console.log(direction)
+        if (direction == "up") {
+            console.log("inside if")
+            for (let i = 0; i < snakePos.length; i++) {
+                console.log('inn')
+                this.table.rows[snakePos[i][1]-1 + 1].cells[snakePos[i][0]-1].className = 'snake_current';
+                
+                this.table.rows[snakePos[i][1]-1 -1].cells[snakePos[i][0]-1].className = '';
+                snakePos[i] = [snakePos[i][0]-1, snakePos[i][1]-1] 
+            }
+        }
     }
 } 
 class Apple extends Grid {
     constructor() {
         super();
-        console.log("abs")
-        console.log(this.lengthInput)
+    }
+
+    SpawnApple(restricted) {
+        console.log('in')
+        this.table = document.getElementById("table")
+        let X = Math.floor(Math.random() * (this.widthInput.value));
+        let Y = Math.ceil(Math.random() * (this.lengthInput.value));
+        for (let i = 0; i < restricted.length; i++) {
+            if (X == restricted[i][0] && Y == restricted[i][1]){
+                console.log("x:" + X + " y:" + Y + " it was the same")
+                X = -1
+                Y = -1
+                this.SpawnApple(restricted);
+            }
+        }
+        console.log(X, Y)
+        try {
+            this.table.rows[Y-1].cells[X-1].className = "apple"
+        } catch {        }
+        
+        
     }
 
 } 
